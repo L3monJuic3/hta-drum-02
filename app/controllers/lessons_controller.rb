ad4ba0dd55a93f022ebd589a1565809272692af7
hamed aljamry <hamed970@hotmail.com>
2022-11-18 17:31:58 +0000

    params

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,18 @@
 class LessonsController < ApplicationController
   def index
-    @lessons = Lesson.all
+    @lesson = Lesson.all
+  end
+
+  def show
+    @lesson = Lesson.find(params[:lesson_id])
+  end
+
+
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :category, :description, :price, :prepare_time, :photo)
   end
 
 end
