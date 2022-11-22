bf91d1a75a7f9d6b366aabbbe2c522e58a345649
ethan lane <ethan.lane@outlook.co.nz>
2022-11-22 12:18:58 +0000

    Fixed email index issue with device and added simple form configuration

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :host
-  belong_to :lesson
+  belongs_to :lesson
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroy all data in database..."
+Lesson.destroy_all
+Booking.destroy_all
+Host.destroy_all
+User.destroy_all
+
+puts "Creating a new database..."
+
+host = User.create!(
+  email: "host@lewagon.com",
+  password: "lewagon",
+  first_name: "host",
+  last_name: "lewagon",
+  phone_number: "07595096963",
+  is_host: true
+)
+
+visitor = User.create!(
+  email: "visitor@lewagon.com",
+  password: "lewagon",
+  first_name: "visitor",
+  last_name: "lewagon",
+  phone_number: "07595096963",
+  is_host: false
+)
