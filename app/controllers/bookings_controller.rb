33a16462cb42c73a1b1ac44b0ca14389dba819f9
ethan lane <ethan.lane@outlook.co.nz>
2022-12-22 22:45:30 +0000

    update bookings controller for time slots

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,34 @@
 class BookingsController < ApplicationController
-  def user_bookings
-    @bookings = Booking.where(user_id: current_user)
-    @current_bookings = (@bookings.where(booking_date: [Date.today..Date.today + 1000.days])).ordered
-    @past_bookings = (@bookings.where(booking_date: [Date.today - 1000.days..Date.today])).ordered
-    @all = @bookings.all
+  before_action :set_booking, only: [:show, :edit, :update, :destroy, :checkout]
+
+  def new
+    if current_user.nil?
+      redirect_to new_user_session_path
+    else
+      @lesson = Lesson.find(params[:lesson_id])
+      @slots_available = Slot.available.where(lesson: @lesson)
+      # @slots_available.select! { |slot| slot.is_available == true }
+      @booking = Booking.new
+      # @slots = @activity.slots.where(is_available: true)
+      # raise
+      # @booking.activity = @activity
+    end
   end
+
+  def my_bookings
+    if current_user.nil?
+      redirect_to new_user_session_path
+    else
+      @bookings = Booking.where(user_id: current_user.id)
+      @lesson = Lesson.where(id: current_user.slots.pluck(:lesson_id).uniq)
+    end
+  end
+  # def user_bookings
+  #   @bookings = Booking.where(user_id: current_user)
+  #   @current_bookings = (@bookings.where(booking_date: [Date.today..Date.today + 1000.days])).ordered
+  #   @past_bookings = (@bookings.where(booking_date: [Date.today - 1000.days..Date.today])).ordered
+  #   @all = @bookings.all
+  # end
 
   def create
     @booking = Booking.new(booking_params)
@@ -12,7 +36,8 @@ class BookingsController < ApplicationController
     @booking.lesson = @lesson
     @booking.user = current_user
     if @booking.save!
-      redirect_to user_bookings_path
+      @booking.slot.update(is_available: false)
+      redirect_to lessson_bookings_path(lesson.id, booking.id)
     else
       render 'lessons/show', status: :unprocessable_entity
     end
@@ -29,4 +54,9 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking_date).permit(:booking)
   end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
 end
